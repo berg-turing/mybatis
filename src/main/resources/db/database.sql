6530ebe3a317a6592823c7049d42f1bb22f3be4c
berg-turing <993450432@qq.com>
2018-07-26 11:51:34 +0800

    完善sql代码

--- src/main/resources/db/database.sql
+++ src/main/resources/db/database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `mybatis` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `mybatis`;
 -- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: mybatis
@@ -175,4 +177,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-26 11:38:18
+-- Dump completed on 2018-07-26 11:50:05
